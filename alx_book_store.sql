72ac15544063c99888fea9bebd2b121bd9a0c108
setordzijoel <setordzijoel@gmail.com>
2025-08-17 11:00:00 +0000

    Modify alx_book_store.sql file.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXIST alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE TABLE IF NOT EXIST Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INTEGER PRIMARY KEY NOT NULL,
     title VARCHAR(130),
     author_id INTEGER,
@@ -9,26 +9,26 @@ CREATE TABLE IF NOT EXIST Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE IF NOT EXIST Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INTEGER PRIMARY KEY NOT NULL,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE IF NOT EXIST Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INTEGER PRIMARY KEY NOT NOT,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE IF NOT EXIST Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INTEGER PRIMARY KEY NOT NULL,
     customer_id INTEGER,
     order_date DATE
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXIST Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INTEGER PRIMARY KEY NOT NOT,
     order_id INTEGER,
     book_id INTEGER,
