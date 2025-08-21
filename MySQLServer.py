09e04a9a5dc6855c2e7d17501abda11c50b2d9c0
setordzijoel <setordzijoel@gmail.com>
2025-08-21 07:27:36 +0000

    Add and modify MYSQLServer.py file.

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    # Connect to the database server.
+    conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "password"
+    )
+
+    if conn.is_connected():
+        cur = conn.cursor()
+
+        # Create database if it does not exist
+        cur.execute("CREATE DATABASE IF NOT EXIST alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+except Error as e:
+    print(f"Error while connecting: {e}")
+finally:
+    # Close connection
+    if conn.is_connected():
+        cur.close()
+        conn.close()
